0db91c6ea22e7b190d45c964df247442dd7898e3
rishabh777ry <rishabh777ry@gmail.com>
2025-02-03 01:25:11 +0530

    second commit

--- app/src/main/java/com/example/digiwhatsapp/MainActivity.kt
+++ app/src/main/java/com/example/digiwhatsapp/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.digiwhatsapp
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,5 +24,31 @@ class MainActivity : AppCompatActivity() {
         val viewPager = findViewById<ViewPager2>(R.id.viewPager)
         val adapter = ViewPagerAdapter(this)
         viewPager.adapter = adapter
+
+        val chats = findViewById<ImageView>(R.id.chat)
+        val status = findViewById<ImageView>(R.id.status)
+        val calls = findViewById<ImageView>(R.id.call)
+
+        val icons = listOf(chats, status, calls)
+
+        // Default selection
+        chats.isSelected = true
+
+        fun selectTab(index: Int) {
+            icons.forEach { it.isSelected = false }
+            icons[index].isSelected = true
+            viewPager.currentItem = index
+        }
+
+        chats.setOnClickListener { selectTab(0) }
+        status.setOnClickListener { selectTab(1) }
+        calls.setOnClickListener { selectTab(2) }
+
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                selectTab(position)
+            }
+        })
     }
 }
